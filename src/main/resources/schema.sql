802689da3c18d44a8d6cec9728ce567fda882899
CoderNoOne <firelight.code@gmail.com>
2026-06-06 15:48:20 +0200

    add Liquibase changelogs for database schema; create sequences, tables, and update application configuration

--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists travels_db.shedlock (
-    name varchar(255) not null,
-    lock_until timestamp not null,
-    locked_at timestamp not null default current_timestamp,
-    locked_by varchar(255) not null,
-    primary key (name)
-);
